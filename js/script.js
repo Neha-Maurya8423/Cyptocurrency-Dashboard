fa1294ca00c99f65c0cf29c57e4514a5f9613eb5
Neha <nehamaurya2572000@gmail.com>
2024-01-25 00:49:47 +0530

    soted data by percentage

--- js/script.js
+++ js/script.js
@@ -1,50 +1,53 @@
-
-// function fetch_live_currency(){
-//  var response;
-// const key = "3cf227b4196e023696b8a5681554ba15";
-// let apiUrl = `http://api.coinlayer.com/live?access_key=${key}`;
-// fetch(apiUrl)
-//   .then((response) => {
-//     return response.json();
-//   })
-//   .then((data) => {
-//     response = data;
-
-//     for (let keys in response.rates) {
-//       document.querySelector(
-//         ".table-body"
-//       ).innerHTML += `<tr><td> ${response.success}</td><td> ${response.timestamp}</td><td> ${keys}</td><td> ${response.rates[keys]}</td></tr>`;
-//     }
-//     console.log(data);
-//   })
-//   .catch((error) => {
-//     console.error("Error fetching currency data:", error);
-//   });
-// }
-
- //https://api.coinpaprika.com/v1/global
-    var response;
-    const key = "3cf227b4196e023696b8a5681554ba15";
-    let apiUrl = `https://api.coingecko.com/api/v3/coins/markets?vs_currency=inr`;
-    fetch(apiUrl)
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        console.log(data);
-
-        for (let i in data) {
-          document.getElementById("table-body").innerHTML += `<tr>
-            <td> <div>${data[i].name }</div></td>
-            <td><img  class="img-fluid currency_symbol" src="${data[i].image }"></td>
-            <td> <div> ${data[i].max_supply} </div> </td>
-            <td> <div> ${data[i].current_price} </div> </td>
-            <td> <div> ${data[i].price_change_24h} </div> </td>
-            <td> <div> ${data[i].price_change_percentage_24h} </div> </td>
+function tableData(sortedData) {
+  for (let i in sortedData) {
+    let color;
+    let change = parseFloat(sortedData[i].price_change_24h).toFixed(2);
+    let percentage = parseFloat(
+      sortedData[i].price_change_percentage_24h
+    ).toFixed(2);
+    if (sortedData[i].price_change_24h > 0) {
+      color = "text-success fa-solid fa-arrow-up";
+    } else {
+      color = "text-danger  fa-solid fa-arrow-down";
+    }
+    table_row = `<tr">
+            <td> <div>${sortedData[i].name}</div></td>
+            <td><img  class="img-fluid currency_symbol" src="${sortedData[i].image}"></td>
+            <td> <div> ${sortedData[i].max_supply} </div> </td>
+            <td> <div> ${sortedData[i].current_price} </div> </td>
+            <td> <div class="sortedData_color  ${color}">&nbsp${change}(${percentage} %)</div> </td> 
           </tr>`;
-        }
-        console.log(data);
-      })
-      .catch((error) => {
-        console.error("Error fetching currency data:", error);
-      });
+    document.getElementById("table-body").innerHTML += table_row;
+  }
+}
+function sortDataByPercentageChange(data) {
+  for (let i = 0; i < data.length; i++) {
+    for (let j = 1; j < data.length - 1; j++) {
+      if (
+        data[j].price_change_percentage_24h <
+        data[j - 1].price_change_percentage_24h
+      ) {
+        let temp = data[j - 1];
+        data[j - 1] = data[j];
+        data[j] = temp;
+      }
+    }
+  }
+  return data;
+}
+function fetchData() {
+  let apiUrl = `https://api.coingecko.com/api/v3/coins/markets?vs_currency=inr`;
+  fetch(apiUrl)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      const sortedData = sortDataByPercentageChange(data);
+      tableData(sortedData);
+      console.log(data);
+    })
+    .catch((error) => {
+      console.error("Error fetching currency data:", error);
+    });
+}
+      fetchData();
